6a10644698376f5d577b7278d48068a48fb3cf90
Vaibhawk <vaibhawk@in-vaibhawk.local>
2017-11-16 18:37:42 +0530

    Query filter

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import matplotlib
 pd.set_option('display.width', 1000)
 
 df = pd.read_csv('train.csv')
 
-print(df)
+# cleaning
+df = df.drop(['Cabin'], axis=1)
+df = df.dropna()
 
+male_survivors = df.query("Sex == 'male' & Survived == '1'")
+
+print(male_survivors)
